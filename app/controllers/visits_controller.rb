1ec204c66485eba7d32d602fb69ba68e13f09fda
Carlos Rodriguez <ucroxlive@gmail.com>
2020-06-27 23:55:09 -0500

    Se crea el metodo create

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -8,4 +8,18 @@ class VisitsController < ApplicationController
     @visit = Visit.new
   end
 
+  def create
+    @visit = Visit.new(visit_params)
+    if @visit.save
+      redirect_to visits_path, notice: "La fecha se agrego con exito!"
+    else
+      render :new
+    end
+  end
+
+  private
+  def visit_params
+    params.require(:visit).permit(:date)
+  end
+
 end
