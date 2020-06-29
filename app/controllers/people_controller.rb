e8d98731a3ee62cc20cfeb38863b7b2849cbbca8
Carlos Rodriguez <ucroxlive@gmail.com>
2020-06-28 22:25:35 -0500

    Crea el metodo create y su vista

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -8,4 +8,18 @@ class PeopleController < ApplicationController
     @person = Person.new
   end
 
+  def create
+    @person = Person.new(people_params)
+    if @person.save
+      redirect_to people_path, notice: "El asesor(@) fue ingresado con exito!"
+    else
+      render :new
+    end
+  end
+
+  private
+  def people_params
+    params.require(:person).permit(:cedula, :name, :last_name)
+  end
+
 end
