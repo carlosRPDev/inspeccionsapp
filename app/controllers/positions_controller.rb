532a8b3050306688ade62e551c8fb96b4c136585
Carlos Rodriguez <ucroxlive@gmail.com>
2020-07-03 12:27:35 -0500

    Crea el modelo create de position

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -7,4 +7,19 @@ class PositionsController < ApplicationController
   def new
     @position = Position.new
   end
+
+  def create
+    @position = Position.new(position_params)
+    if @position.save
+      redirect_to positions_path, notice: "El tipo de cargo ha sido ingresado con exito!"
+    else
+      render :new
+    end
+  end
+
+  private
+  def position_params
+    params.require(:position).permit(:description)
+  end
+
 end
